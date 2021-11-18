fe5a966cddfaf84020db00a5434aaf97acf4d0e3
Claire <claire@Claires-MacBook-Pro-3.local>
2021-11-18 13:53:36 -0500

    added 5-day forecast output

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,8 @@ var currentTemp = document.querySelector("#current-temp");
 var currentWind = document.querySelector("#current-wind");
 var currentHumidity = document.querySelector("#current-humidity");
 var currentUvIndex = document.querySelector("#current-uv");
-var searchResults = document.querySelector("#results");
+var searchResults = document.querySelector("#search-results");
+
 
 var getCoordinates = function(city) {
     // create API url
@@ -20,6 +21,7 @@ var getCoordinates = function(city) {
                 var cityLon = data[0].lon;
                 // pass coordinates to weather function
                 getCurrentWeather(cityLat, cityLon);
+                getFutureWeather(cityLat, cityLon);
             });
         } 
     });
@@ -27,17 +29,59 @@ var getCoordinates = function(city) {
 
 var getCurrentWeather = function(lat, lon) {
     // create API url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=hourly,daily&appid=cf497bbc93c8c77ef641ec280f1648f7";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&exclude=hourly,minutely&appid=cf497bbc93c8c77ef641ec280f1648f7";
     
     // fetch API url
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
                 // get values for weather objects
-                console.log(data);
-                console.log(data.current.temp);
+                var uvIndex = data.current.uvi;
                 currentTemp.textContent = "Temp: " + data.current.temp + " °F";
                 currentWind.textContent = "Wind: " + data.current.wind_speed + " MPH";
+                currentHumidity.textContent = "Humidity: " + data.current.humidity + " %";
+                currentUvIndex.textContent = uvIndex;
+                checkUv(uvIndex);
+            });
+        } 
+    });
+};
+
+var checkUv = function(uvi) {
+    if (uvi < 3) {
+        currentUvIndex.classList.add("favorable");
+    } else if (uvi < 6) {
+        currentUvIndex.classList.add("moderate");
+    } else {
+        currentUvIndex.classList.add("severe");
+    }
+};
+
+var getFutureWeather = function(lat, lon) {
+    // create API url
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=cf497bbc93c8c77ef641ec280f1648f7";
+
+    // fetch API url
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log(data);
+                // use data to get today's date
+                var dateTime = data.list[0].dt_txt;
+                var date = dateTime.split(" ", 2);
+                weatherHeader.textContent = weatherHeader.textContent + " (" + date[0] + ")";
+                // loop to get values for weather objects for each card
+                var index = 6;
+                for (var i = 1; i < 6; i++) {
+                    dateTime = data.list[index].dt_txt;
+                    date = dateTime.split(" ", 2);
+                    document.querySelector("#card-date-" + i).textContent = date[0];
+                    document.querySelector("#card-temp-" + i).textContent = "Temp: " + data.list[index].main.temp + " °F";
+                    document.querySelector("#card-wind-" + i).textContent = "Wind: " + data.list[index].wind.speed + " MPH";
+                    document.querySelector("#card-humidity-" + i).textContent = "Humidity: " + data.list[index].main.humidity + " %";
+                    index = index + 8;
+                }
+                
             });
         } 
     });
